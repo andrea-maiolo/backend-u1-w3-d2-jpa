004b98c01572178479d565e6da61d5d9caa2c2c8
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-01 16:02:38 +0200

    connesso db

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,69 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import net.datafaker.Faker;
+import org.example.dao.EventoDao;
+import org.example.entities.Evento;
+import org.example.entities.TipoEvento;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class Application {
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u1w3d2gestioneeventi");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        EventoDao edao = new EventoDao(em);
+
+        Evento compleanno = new Evento("compleanno",
+                LocalDate.of(2025, 04, 03), "descrizione di compleanno"
+                , TipoEvento.PUBBLICO, 15);
+
+        Evento meeting = new Evento("meeting",
+                LocalDate.of(2025, 07, 23), "meeting di lavoro"
+                , TipoEvento.PRIVATO, 6);
+
+        //edao.save(compleanno);
+//        Evento eventoFromDb = edao.getById(1);
+//        System.out.println(eventoFromDb);
+
+        // edao.delete(1);
+
+        // edao.save(meeting);
+//        try {
+//            Event eventFromDB = edao.findById(2);
+//            System.out.println(eventFromDB);
+//
+//        } catch (NotFoundException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+
+
+        Faker faker = new Faker(Locale.ITALY);
+        Random random = new Random();
+
+        Supplier<Evento> eventSupp = () -> {
+            String sTitolo = faker.esports().game();
+            int sNMP = random.nextInt(3, 20);
+            String sDescri = faker.esports().event();
+            TipoEvento sTipo = TipoEvento.PUBBLICO;
+            LocalDate sLd = LocalDate.of(2025, 11, 5);
+
+            Evento eventSupplied = new Evento(sTitolo, sLd, sDescri, sTipo, sNMP);
+            return eventSupplied;
+        };
+
+        for (int i = 0; i < 7; i++) {
+            edao.save(eventSupp.get());
+        }
+
+        em.close();
+        emf.close();
+
     }
 }
---
+++ src/main/java/org/example/entities/Evento.java
@@ -0,0 +1,87 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "eventi")
+public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String titolo;
+    private LocalDate dataEvento;
+    private String descrizione;
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipoEvento;
+    private int numeroMassimoPartecipanti;
+
+    public Evento() {
+    }
+
+    public Evento(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti) {
+        this.dataEvento = dataEvento;
+        this.titolo = titolo;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public TipoEvento getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public void setTipoEvento(TipoEvento tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+
+    public int getNumeroMassimoPartecipanti() {
+        return numeroMassimoPartecipanti;
+    }
+
+    public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "id=" + id +
+                ", titolo='" + titolo + '\'' +
+                ", dataEvento=" + dataEvento +
+                ", descrizione='" + descrizione + '\'' +
+                ", tipoEvento=" + tipoEvento +
+                ", numeroMassimoPartecipanti=" + numeroMassimoPartecipanti +
+                '}';
+    }
+}
